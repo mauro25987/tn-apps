69ff905a1808a5f9012d7b72926096314e435889
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2023-02-14 17:58:17 -0300

    add sources callfinder

--- src/routes/callfinder.py
+++ src/routes/callfinder.py
@@ -2,6 +2,25 @@ from flask import Blueprint, render_template
 
 callfinder = Blueprint('callfinder', __name__, url_prefix='/callfinder')
 
+ovh_data = {
+    "host": "192.168.70.13",
+    "user": "internalreports",
+    "password": "internalreports",
+    "database": "asterisk",
+}
+
+ovh2_data = {
+    "host": "192.168.60.15",
+    "user": "internalreports",
+    "password": "internalreports",
+    "database": "asterisk"
+}
+
+servers = {
+    "ovh":  ovh_data,
+    "ovh2": ovh2_data,
+}
+
 
 @callfinder.route('/')
 def index():
